69b90bbb774057f65f82c368522e51a3ba33127b
robertatatiana <robertatatiana00@gmail.com>
2024-01-20 15:44:45 -0300

    feat: Adicionando doc Swagger

--- RegistroAlunos/Controllers/AlunoController.cs
+++ RegistroAlunos/Controllers/AlunoController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RegistroAlunos.Models;
 using RegistroAlunos.Repositorios.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace RegistroAlunos.Controllers
 {
@@ -15,6 +17,10 @@ namespace RegistroAlunos.Controllers
             _alunoRepositorio = alunoRepositorio;
         }
 
+        /// <summary>
+        /// Obtém todos os alunos cadastrados.
+        /// </summary>
+        /// <returns>Lista de alunos</returns>
         [HttpGet]
         public async Task<IActionResult> BuscarTodosAlunos()
         {
@@ -22,14 +28,29 @@ namespace RegistroAlunos.Controllers
             return Ok(alunos);
         }
 
+        /// <summary>
+        /// Obtém um aluno pelo seu ID.
+        /// </summary>
+        /// <param name="id">ID do aluno</param>
+        /// <returns>O aluno correspondente ao ID</returns>
         [HttpGet("{id}")]
         public async Task<IActionResult> BuscarAlunoPorId(int id)
         {
             Aluno aluno = await _alunoRepositorio.BuscarAlunoPorId(id);
 
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+
             return Ok(aluno);
         }
 
+        /// <summary>
+        /// Adiciona um novo aluno.
+        /// </summary>
+        /// <param name="aluno">Dados do aluno a ser adicionado</param>
+        /// <returns>O aluno recém-adicionado</returns>
         [HttpPost]
         public async Task<IActionResult> AdicionarAluno([FromBody] Aluno aluno)
         {
@@ -38,18 +59,39 @@ namespace RegistroAlunos.Controllers
             return Ok(alunoAdicionado);
         }
 
+        /// <summary>
+        /// Atualiza um aluno pelo seu ID.
+        /// </summary>
+        /// <param name="id">ID do aluno a ser atualizado</param>
+        /// <param name="aluno">Dados do aluno atualizado</param>
+        /// <returns>O aluno atualizado</returns>
         [HttpPut("{id}")]
         public async Task<IActionResult> AtualizarAluno([FromBody] Aluno aluno, int id)
         {
             Aluno alunoAtualizado = await _alunoRepositorio.AtualizarAluno(aluno, id);
 
+            if (alunoAtualizado == null)
+            {
+                return NotFound();
+            }
+
             return Ok(alunoAtualizado);
         }
 
+        /// <summary>
+        /// Deleta um aluno pelo seu ID.
+        /// </summary>
+        /// <param name="id">ID do aluno a ser deletado</param>
+        /// <returns>True se o aluno foi deletado com sucesso</returns>
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeletarAluno(int id)
         {
             bool alunoDeletado = await _alunoRepositorio.DeletarAluno(id);
+
+            if (!alunoDeletado)
+            {
+                return NotFound();
+            }
 
             return Ok(alunoDeletado);
         }
--- RegistroAlunos/Program.cs
+++ RegistroAlunos/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using RegistroAlunos.Data;
 using RegistroAlunos.Repositorios;
 using RegistroAlunos.Repositorios.Interfaces;
+using System.Reflection;
 
 internal class Program
 {
@@ -14,7 +16,13 @@ internal class Program
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen(x =>
+        {
+            x.SwaggerDoc("v1", new OpenApiInfo { Title = "RegistroAlunos", Version = "v1" });
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            x.IncludeXmlComments(xmlPath);
+        });
 
         //adicionar o contexto do banco de dados
         builder.Services.AddEntityFrameworkSqlServer().AddDbContext<AppDbContext>(
